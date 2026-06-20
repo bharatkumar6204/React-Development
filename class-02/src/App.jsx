01d7d4d47edee32975ddab1bbb58bceb77ca1014
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-06-20 23:26:16 +0530

    Hooks in Counter Number

---
+++ class-02/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [num, setNum] = useState(0)
+
+  return (
+    <div>
+      <h3>Number is {num}</h3>
+      <button onClick={()=>setNum(num+1)}>Increment</button>
+      <button onClick={()=>setNum(num-1)}>Decrement</button>
+      
+    </div>
+  )
+}
+
+export default App
