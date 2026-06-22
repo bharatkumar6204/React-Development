d76b7802b7b2f7613be6029a5bab5b07e117a9cc
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-06-22 22:41:25 +0530

    tailwindcss setup

---
+++ class-03/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=' font-bold text-2xl text-cyan-500 text-center'>
+      <h1>Hello Tailwind css</h1>
+      
+    </div>
+  )
+}
+
+export default App
