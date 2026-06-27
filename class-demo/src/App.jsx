8a0e508449f2c66de3a56300a5ce0d6b7e6fd848
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-06-27 08:04:22 +0530

    practice

--- class-demo/src/App.jsx
+++ class-demo/src/App.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [num, setNum] = useState('')
   const submitHandler = (e)=>{
     e.preventDefault()
     console.log('Form submited');
+    setNum('')
+    
   }
   return (
     <div>
       <form onSubmit={(e)=>{
         submitHandler(e)
-      }}>
-       <input className='px-4 py-3 m-5 text-xl font-semibold text-cyan-600' type="text" placeholder='Enter your name' />
-      <button className='px-4 py-3 text-xl font-semibold   bg-cyan-600 rounded'>Submit</button>
+      }} >
+        <input
+        value={num}
+        onChange={(e)=>{
+          setNum(e.target.value)
+        }}
+         type="text"
+          placeholder='Enter your name'
+         />
+        <button>sumit</button>
       </form>
       
     </div>
