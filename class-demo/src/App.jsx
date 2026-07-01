b79ecc6518e5f210fbd4cabd3938ccc2d5660653
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-07-01 22:36:48 +0530

    practice

--- class-demo/src/App.jsx
+++ class-demo/src/App.jsx
@@ -1,13 +1,37 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [first, setfirst] = useState(second)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    console.log('form submited');
+    setName('')
+    setEmail('')
+    
+  }
   return (
     <div>
-      <form onSubmit={()=>{
-
+      <form onSubmit={(e)=>{
+        submitHandler(e)
       }}>
-      <input type="text" placeholder='Enter your name' /><br />
+      <input 
+      value={name}
+      onChange={(e)=>{
+        setName(e.target.value)
+      }}
+      type="text"
+       placeholder='Enter your name'
+        /><br />
+      <input
+      value={email}
+      onChange={(e)=>{
+        setEmail(e.target.value)
+      }}
+       type="email" 
+       placeholder='Enter email'
+        /><br />
       <button>Submit</button>
       </form>
       
